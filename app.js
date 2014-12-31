c16f014257e13b965cd60db6586bc75c548c38c7
mateoclarke <code@mateoclarke.com>
2014-12-31 17:19:31 -0600

    Level 3.1: User Params - Reading from the URL

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var port = 3000
 
+var blocks = {
+	'Fixed': 'Fastened securely in position',
+	'Movable': 'Capable of being moved',
+	'Rotating': 'Moving in a circle around its center'
+};
+
 // We require and use our logger module in our application.
 // './' specifies that this is a local module and not a Node module.
 var logger = require('./logger');
@@ -19,16 +25,36 @@ app.use(express.static('public'));
 	// });
 
 app.get('/blocks', function(request, response) {
-	var blocks = ['Fixed', 'Movable', 'Rotating'];
-	response.json(blocks);
+	// var blocks = ['Fixed', 'Movable', 'Rotating'];
+	// use request.query to access query strings
+	if (request.query.limit >= 0) {
+		response.json(blocks.slice(0, request.query.limit))
+	} else {
+		response.json(blocks);
+	}
 
-	/* This does the same as above: ---
+	/* This does the same thing as: ---
+			response.json(blocks)
 		The json() function reads better
 		when all we respond with is JSON.
 		The same as send(), for Objects and
 		Arrays 
 		*/
 	// response.send(blocks);
+});
+
+// CREATING DYNAMIC ROUTES
+// Placeholders can be used to name arguments part of the URL path
+app.get('/blocks/:name', function(request, response){
+	// We use request.params.name to look up the Block's description
+	var description = blocks[request.params.name];
+	if (!description) {
+		response.status(404).json('No description found for ' + request.params.name);
+	} else {	
+		// Responding with description and proper status code
+		response.json(description);
+	}
+
 });
 
 app.get('/blocks2', function(request, response){
