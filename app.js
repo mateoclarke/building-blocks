1bb6b63a355c6d987cd1e034a09243ec375779a6
mateoclarke <code@mateoclarke.com>
2015-01-01 22:22:06 -0600

    Level 4.1: POST Requests

--- app.js
+++ app.js
@@ -2,74 +2,95 @@ var express = require('express');
 var app = express();
 var port = 3000
 
+
 var blocks = {
 	'Fixed': 'Fastened securely in position',
 	'Movable': 'Capable of being moved',
 	'Rotating': 'Moving in a circle around its center'
 };
 
-// We require and use our logger module in our application.
-// './' specifies that this is a local module and not a Node module.
+var locations = {
+	'Fixed': 'First floor', 'Movable': 'Second Floor', 'Rotating': 'Penthouse'
+};
+
+// MIDDLEWARE
 var logger = require('./logger');
 app.use(logger);
-
-// The app.use function adds middleware to the application stack.
-// static() function is middleware that serves files from the public folder.
 app.use(express.static('public'));
 
-	/* This does the same as above. The index.html file is served from Express.
-	__dirname is the name of the directory the currently executing script resides in.
-	*/
-	// app.get('/', function(request, response) {
-	// 	response.sendFile(__dirname + '/public/index.html');
-	// });
+// MIDDLEWARE for Parsing Form Data
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 
+// PROPERTIES ON THE REQUEST OBJECT
+// These can be accessed from all subsequent routes in the application
+app.param('name', function (request, response, next) {
+	var name = request.params.name;
+	var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+	// This is accessible from other routes in the application
+	request.blockName = block;
+
+	next();
+});
+
+
+// POST ROUTE
+// Routes can take multiple handlers as arguemnts and will call them sequentially.
+// Using multiple route handlers is useful for re-using middleware that load resources, 
+// perform validations, authentication, etc.
+app.post('/blocks', parseUrlencoded, function(request, response){
+	
+	// Form submitted date can be accessed through request.body Object
+	var newBlock = request.body;
+
+	// The form elements are parsed to object properties, name and description
+	blocks[newBlock.name] = newBlock.description;
+
+	// The response includes proper 201 Created status code and responds with the block name.
+	response.status(201).json(newBlock.name);
+});
+
+// ROUTES
 app.get('/blocks', function(request, response) {
-	// var blocks = ['Fixed', 'Movable', 'Rotating'];
-	// use request.query to access query strings
 	if (request.query.limit >= 0) {
 		response.json(blocks.slice(0, request.query.limit))
 	} else {
-		response.json(blocks);
+		response.json(Object.keys(blocks));
 	}
-
-	/* This does the same thing as: ---
-			response.json(blocks)
-		The json() function reads better
-		when all we respond with is JSON.
-		The same as send(), for Objects and
-		Arrays 
-		*/
-	// response.send(blocks);
 });
 
-// CREATING DYNAMIC ROUTES
-// Placeholders can be used to name arguments part of the URL path
+// DYNAMIC ROUTES
 app.get('/blocks/:name', function(request, response){
-	// We use request.params.name to look up the Block's description
-	var description = blocks[request.params.name];
+	var description = blocks[request.blockName];
 	if (!description) {
 		response.status(404).json('No description found for ' + request.params.name);
 	} else {	
-		// Responding with description and proper status code
 		response.json(description);
 	}
-
 });
 
+app.get('/locations/:name', function(request, response){
+	var description = locations[request.blockName];
+	if (!description) {
+		response.status(404).json('No description found for ' + request.params.name);
+	} else {	
+		response.json(description);
+	}
+});
+
+// ROUTE WITH HTML RESPONSE 
 app.get('/blocks2', function(request, response){
 	var blocks2 = '<ul><li>Fixed</li><li>Movable</li></ul>';
 	response.send(blocks2);
 })
 
+// REDIRECTING ROUTE
 app.get('/blocks3', function(request, response) {
-	// The redirect() function sets the proper response headers
-	// optional status code can be passed as the first argument to redirct()
-	// examples uses 301 Moved Permanently 
-	// default is status code 302 Moved Temporarily
 	response.redirect(301, '/parts');
 });
 
+// LISTENING PORT & MESSAGE
 app.listen(port, function(){
 	console.log('Listening on http://localhost:' + port + '/');
 });
--- public/client.js
+++ public/client.js
@@ -4,11 +4,35 @@ $(function(){
 	// appends the results to block-list
 	$.get('/blocks', appendToList);
 
+	// Data is sent in a POST request to the /blocks endpoint
+	$('form').on('submit', function(event){
+		event.preventDefault();
+		var form = $(this);
+		// The serialize() functions transforms form data to URL-encoded notation
+		// so our Express app can parse it back to Javascript
+		var blockData = form.serialize();
+
+		$.ajax({
+			type: 'POST',
+			url: '/blocks',
+			data: blockData
+		}).done(function(blockName){
+			// The appendToList() function expects an array of Blocks
+			// so we wrap our arguement in an array.
+			// This way we avoid changing our original function to expect different types of arguements
+			appendToList([blockName]);
+			// We must clear the input text fields after posting the form
+			form.trigger('reset');
+		});
+	});
+
 	function appendToList(blocks) {
 		var list = [];
 		for(var i in blocks){
-			list.push($('<li>', {text: blocks[i] }));
+			block = blocks[i];
+			content = '<a href="/blocks/'+block+'">'+block+'</a>';
+			list.push($('<li>', {html: content }));
 		}
 		$('.block-list').append(list);
 	}
-})
+});
